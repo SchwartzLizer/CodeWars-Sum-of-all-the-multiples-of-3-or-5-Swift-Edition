168a31334fea84413512396a3b73a6dd06c8012f
Tanatip Denduangchai <De.tanatip@outlook.com>
2019-08-20 11:47:43 +0700

    Initial Commit

---
+++ CodeWars : Sum of all the multiples of 3 or 5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeWars : Sum of all the multiples of 3 or 5
+//
+//  Created by Tanatip Denduangchai on 8/20/19.
+//  Copyright © 2019 Tanatip Denduangchai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
